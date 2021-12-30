07744ea49227ec70491dc5f30235fcb1a837eae5
Aboubacar Toure <aboubacar212toure@gmail.com>
2021-12-30 16:25:11 +0100

    test

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -16,20 +16,8 @@ class PageController extends AppController
 
 
     public function home(){
-        $qrCode = new QrCode();
-        $qrCode 
-            -> setText ( 'https://www.lesoir.be/' )
-            -> setSize ( 100 )
-            -> setPadding ( 40 )
-            -> setErrorCorrection ( 'HIGH' )
-            -> setForegroundColor ( array ( 'r' => 0 , 'g' => 0 , 'b' => 0 , 'a' => 0 ))
-            -> setBackgroundColor ( array ( 'r' => 255 , 'g' => 255 , 'b' => 255 , 'a' => 0 ))
-            -> setLabel ( 'Visit this website' )
-            -> setLabelFontSize ( 16 )
-            -> setImageType (QrCode::IMAGE_TYPE_PNG )
-        ;
-        $imgQrCode =  '<img src="data:' . $qrCode -> getContentType (). ';base64,' . $qrCode -> generate (). '" />' ;
-    $this->set(compact('imgQrCode'));
+     
+        
     }
     public function about(){
 
