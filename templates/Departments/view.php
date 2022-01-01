b23131390dd5414cdf7696a4a78c58440d9a9613
lswart <lauren.swart@gmail.com>
2022-01-01 13:27:07 +0100

    index, view and  css

--- templates/Departments/view.php
+++ templates/Departments/view.php
@@ -6,85 +6,88 @@
 ?>
 
 
-        <div class="departments view content">
-            <h3><?= __($department->dept_name) ?></h3>
-            <?= $this->Html->image(h('departments/'.$department->picture), [
-                                    "alt" => __("Photo d'équipe du départment"),
-                                    "class"=>"card-img-top"
-                            ]);?>
-            <table>
-                <tr>
-                    <th><?= __('Numéro de départmement') ?></th>
-                    <td><?= h($department->dept_no) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Description') ?></th>
-                    <td><?= h($department->description) ?></td>
-                </tr>
-                
-                <tr>
-                    <th><?= __('Manager actuel') ?></th>
-                    <td>
-                    	<?= $department->manager->picture ? $this->Html->image('employees/'.$department->manager->picture).'<br>' : ''?>
-                    	<?= $department->manager->first_name.' '.$department->manager->last_name ?>
-                    </td>
-                </tr>
-                <tr>
-                    <th><?= __('Adresse') ?></th>
-                    <td><?= h($department->address) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('ROI') ?></th>
-                    <td><?= $this->Html->link(__('Telecharger le ROI'), '/docs/ROI/'.$department->roi, ['download'=>$department->roi]) ?></td>
-                </tr>
-                
-            </table>
+<div class="departments view content">
+    <h1><?= __($department->dept_name) ?></h3>
+    <?= $this->Html->image(h('departments/'.$department->picture), [
+                            "alt" => __("Photo d'équipe du départment"),
+                            "class"=>"card-img-top"
+                    ]);?>
+    <table>
+        <tr>
+            <th><?= __('Numéro de départmement') ?></th>
+            <td><?= h($department->dept_no) ?></td>
+        </tr>
+        <tr>
+            <th><?= __('Description') ?></th>
+            <td><?= h($department->description) ?></td>
+        </tr>
+        
+        <tr>
+            <th><?= __('Manager actuel') ?></th>
+            <td>
+            	<?= $department->manager->picture ? $this->Html->image('employees/'.$department->manager->picture).'<br>' : ''?>
+            	<?= $department->manager->first_name.' '.$department->manager->last_name ?>
+            </td>
+        </tr>
+        <tr>
+            <th><?= __('Adresse') ?></th>
+            <td><?= h($department->address) ?></td>
+        </tr>
+        <?php //if($this->request->getAttribute('authentication')->getIdentity()->currentDepartment == $department->dept_no) { ?>
+        <tr>
+        	
+            <th><?= __('ROI') ?></th>
+            <td><?= $this->Html->link(__('Telecharger le ROI'), '/docs/ROI/'.$department->roi, ['download'=>$department->roi]) ?></td>
+        </tr>
+       <?php //} ?>
+        
+    </table>
+    
+    <h2><?= __('Managers') ?></h3>
+    
+    <table>
+    	<thead>
+        	<tr>
+        		<th><?= __('Nom') ?></th>
+        		<th><?= __('Date de début') ?></th>
+        		<th><?= __('Date de fin') ?></th>
+        	</tr>
+        </thead>
+        <?php foreach($department->managers as $manager) {?>
+        
+        <tbody>
+        <tr>
+        	<td><?= $manager->first_name.' '.$manager->last_name ?></td>
+            <td><?= $this->Time->format($manager->_joinData->from_date, 'd/M/Y') ?></td>
+            <td><?= $this->Time->format($manager->_joinData->to_date, 'd/M/Y')=='1/1/9999' ? __('Présent') :  $this->Time->format($manager->_joinData->to_date, 'd/M/Y')?></td>
             
-            <h3><?= __('Managers') ?></h3>
-            
-            <table>
-            	<thead>
-                	<tr>
-                		<th><?= __('Nom') ?></th>
-                		<th><?= __('Date de début') ?></th>
-                		<th><?= __('Date de fin') ?></th>
-                	</tr>
-                </thead>
-                <?php foreach($department->managers as $manager) {?>
-                
-                <tbody>
-                <tr>
-                	<td><?= $manager->first_name.' '.$manager->last_name ?></td>
-                    <td><?= $this->Time->format($manager->_joinData->from_date, 'd/M/Y') ?></td>
-                    <td><?= $this->Time->format($manager->_joinData->to_date, 'd/M/Y')=='1/1/9999' ? __('Présent') :  $this->Time->format($manager->_joinData->to_date, 'd/M/Y')?></td>
-                    
-                </tr>
-                <?php } ?>
-                </tbody>
-            </table>
-            
-            <h3><?= __('Postes vacants') ?></h3>
-            
-            <?php if (!empty($department->offers)) { ?>
-            <table>
-            	<thead>
-                	<tr>
-                		<th><?= __('Titre') ?></th>
-                		<th colspan="2"><?= __('Description') ?></th>
-                	</tr>
-                </thead>
-                <?php foreach($department->offers as $offer) {?>
-                
-                <tbody>
-                <tr>
-                	<td><?= __($offer->titleName) ?></td>
-                	<td><?= __($offer->description) ?></td>
-                    <td><?= $this->Html->link(__('Postuler'), ['controller'=>'offers', 'action'=>'apply', $offer->offer_no]) ?></td>
-                </tr>
-                <?php } ?>
-                </tbody>
-            </table>
-            <?php } else {  ?>
-            	<p><?= __('Aucun poste vacant') ?></p>
-            <?php } ?>
-        </div>
+        </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+    
+    <h2><?= __('Postes vacants') ?></h3>
+    
+    <?php if (!empty($department->offers)) { ?>
+    <table>
+    	<thead>
+        	<tr>
+        		<th><?= __('Titre') ?></th>
+        		<th colspan="2"><?= __('Description') ?></th>
+        	</tr>
+        </thead>
+        <?php foreach($department->offers as $offer) {?>
+        
+        <tbody>
+        <tr>
+        	<td><?= __($offer->titleName) ?></td>
+        	<td><?= __($offer->description) ?></td>
+            <td><?= $this->Html->link(__('Postuler'), ['controller'=>'offers', 'action'=>'apply', $offer->offer_no]) ?></td>
+        </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+    <?php } else {  ?>
+    	<p><?= __('Aucun poste vacant') ?></p>
+    <?php } ?>
+</div>
